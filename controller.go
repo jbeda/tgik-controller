920266713c72c1170e05d0864ba5722d794a599c
Joe Beda <joe.github@bedafamily.com>
2017-09-08 14:38:45 -0700

    Update from broadcast

--- controller.go
+++ controller.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	informercorev1 "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	listercorev1 "k8s.io/client-go/listers/core/v1"
 	apicorev1 "k8s.io/client-go/pkg/api/v1"
@@ -14,7 +18,7 @@ import (
 )
 
 const (
-	secretSyncType            = "eightypercent.net/secretsync"
+	secretSyncAnnotation      = "eightypercent.net/secretsync"
 	secretSyncSourceNamespace = "secretsync"
 )
 
@@ -71,9 +75,10 @@ func (c *TGIKController) Run(stop <-chan struct{}) {
 	log.Print("caches are synced")
 
 	// wait until we're told to stop
-	log.Print("waiting for stop signal")
-	<-stop
-	log.Print("received stop signal")
+	// log.Print("waiting for stop signal")
+	// <-stop
+	// log.Print("received stop signal")
+	c.doSync()
 }
 
 func (c *TGIKController) onAdd(obj interface{}) {
@@ -83,7 +88,7 @@ func (c *TGIKController) onAdd(obj interface{}) {
 		runtime.HandleError(err)
 	}
 	log.Printf("onAdd: %v", key)
-	c.handleSecretChange(obj)
+	// c.handleSecretChange(obj)
 }
 
 func (c *TGIKController) onUpdate(oldObj, newObj interface{}) {
@@ -93,7 +98,7 @@ func (c *TGIKController) onUpdate(oldObj, newObj interface{}) {
 		runtime.HandleError(err)
 	}
 	log.Printf("onUpdate: %v", key)
-	c.handleSecretChange(newObj)
+	// c.handleSecretChange(newObj)
 }
 
 func (c *TGIKController) onDelete(obj interface{}) {
@@ -102,7 +107,7 @@ func (c *TGIKController) onDelete(obj interface{}) {
 		runtime.HandleError(err)
 	}
 	log.Printf("onDelete: %v", key)
-	c.handleSecretChange(obj)
+	// c.handleSecretChange(obj)
 }
 
 func (c *TGIKController) handleSecretChange(obj interface{}) {
@@ -118,10 +123,10 @@ func (c *TGIKController) handleSecretChange(obj interface{}) {
 		return
 	}
 
-	if secret.Type != secretSyncType {
-		log.Printf("Skipping secret of wrong type")
-		return
-	}
+	// if secret.Type != secretSyncType {
+	// 	log.Printf("Skipping secret of wrong type")
+	// 	return
+	// }
 
 	log.Printf("Do something with this secret")
 	nsList, err := c.namespaceGetter.Namespaces().List(metav1.ListOptions{})
@@ -136,14 +141,88 @@ func (c *TGIKController) handleSecretChange(obj interface{}) {
 			continue
 		}
 		log.Printf("We should copy %s to namespace %s", secret.ObjectMeta.Name, ns.ObjectMeta.Name)
-		copySecretToNamespace(secret, nsName)
+		c.doSync()
 	}
 }
 
-func (c *TGIKController) copySecretToNamespace(secret *apicorev1.Secret, nsName string) {
-	// TODO:
-	// 1. Make a deep copy of the secret
-	// 2. Remove things like object version that'll prevent us from writing
-	// 3. Write in new namespace
-	// 4. Do a create or update for the new object
+func (c *TGIKController) getSecretsInNS(ns string) ([]*apicorev1.Secret, error) {
+	rawSecrets, err := c.secretLister.Secrets(ns).List(labels.Everything())
+	if err != nil {
+		return nil, err
+	}
+
+	var secrets []*apicorev1.Secret
+	for _, secret := range rawSecrets {
+		if _, ok := secret.Annotations[secretSyncAnnotation]; ok {
+			secrets = append(secrets, secret)
+		}
+	}
+	return secrets, nil
+}
+
+func (c *TGIKController) doSync() {
+	log.Printf("here!")
+	srcSecrets, err := c.getSecretsInNS(secretSyncSourceNamespace)
+	if err != nil {
+		log.Panicf("oh noes! %v", err)
+	}
+
+	rawNamespaces, err := c.namespaceLister.List(labels.Everything())
+	if err != nil {
+		log.Panicf("oh noes! %v", err)
+	}
+	var targetNamespaces []*apicorev1.Namespace
+	for _, ns := range rawNamespaces {
+		if _, ok := ns.Annotations[secretSyncAnnotation]; ok {
+			targetNamespaces = append(targetNamespaces, ns)
+		}
+	}
+
+	for _, ns := range targetNamespaces {
+		c.SyncNamespace(srcSecrets, ns.Name)
+	}
+}
+
+func (c *TGIKController) SyncNamespace(secrets []*apicorev1.Secret, ns string) {
+	// 1. Create/Update all of the secrets in this namespace
+	for _, secret := range secrets {
+		newSecretInf, _ := scheme.Scheme.DeepCopy(secret)
+		newSecret := newSecretInf.(*apicorev1.Secret)
+		newSecret.Namespace = ns
+		newSecret.ResourceVersion = ""
+		newSecret.UID = ""
+
+		_, err := c.secretGetter.Secrets(ns).Create(newSecret)
+		if apierrors.IsAlreadyExists(err) {
+			_, err = c.secretGetter.Secrets(ns).Update(newSecret)
+		}
+		if err != nil {
+			log.Printf("%v", err)
+		}
+	}
+
+	// 2. Delete secrets that have annotation but are not in our src list
+	srcSecrets := sets.String{}
+	targetSecrets := sets.String{}
+
+	for _, secret := range secrets {
+		srcSecrets.Insert(secret.Name)
+	}
+
+	targetSecretList, err := c.getSecretsInNS(ns)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	for _, secret := range targetSecretList {
+		targetSecrets.Insert(secret.Name)
+	}
+
+	deleteSet := targetSecrets.Difference(srcSecrets)
+	for secretName, _ := range deleteSet {
+		log.Printf("Delete %v", secretName)
+		err = c.secretGetter.Secrets(ns).Delete(secretName, nil)
+		if err != nil {
+			log.Printf("%v", err)
+		}
+	}
 }
